e52fe4f5a72498fa3bfbb9c1b4fa796fdb36858d
abd074 <badawyabdulrahman099@gmail.com>
2024-01-19 23:56:49 +0200

    show data

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,41 +1,43 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Movies() {
+  let [trendingMovies, settrendingMovies] = useState([]);
 
-let [trendingMovies, settrendingMovies] = useState([])
-
-  async function getMovies(){
-    
-    
+  async function getMovies() {
     // let response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=5b6d2f21c07b6b480b047d8a71c3824d`)
     // console.log(response.data.results);
 
-
-
-    let {data} = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=5b6d2f21c07b6b480b047d8a71c3824d`)
+    let { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=5b6d2f21c07b6b480b047d8a71c3824d`
+    );
     console.log(data.results);
-    settrendingMovies(data.results)
+    settrendingMovies(data.results);
   }
 
-
-
-  useEffect(()=>{
-
-
-    getMovies()
-
-
-  },[])
-
-
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <div>
-      Movies
+      <div className="container">
+        <div className="row">
+          {trendingMovies.map((movie, i) => (
+            <div key={i} className="col-md-3">
+              <img
+                className="w-100"
+                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                alt=""
+              />
+              <h5>{movie.title}</h5>
+              <p>{movie.overview}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
 
 // https://api.themoviedb.org/3/trending/movie/week?api_key=5b6d2f21c07b6b480b047d8a71c3824d
